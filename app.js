3e350abd3220bec543b7f715acfaeeda3143e42c
JJCazel <jjcazel@gmail.com>
2019-10-19 20:35:00 -0400

    Modify promise chain in app.js

--- app.js
+++ app.js
@@ -27,65 +27,71 @@ app.get('/login', (request, res) => {
 })
 
 let audio_features = []
-
+let tracj_info = []
 app.get('/app', (request, res) => {
-    console.log(request.query)
+    console.log(1 ,request.query)
     // const authorizationCode = request.params.authorizationCode
     // res.send('hello')
     // res.redirect(`https://accounts.spotify.com/authorize?client_id=efe8564cdab24aeda7bf97b81c57683d&response_type=code&redirect_uri=${redirect_uri}`)
-    spotifyApi.authorizationCodeGrant(request.query.code).then(function (data) {
+    spotifyApi.authorizationCodeGrant(request.query.code)
+    .then(function (data) {
         spotifyApi.setAccessToken(data.body.access_token);
         spotifyApi.setRefreshToken(data.body.refresh_token);
         return spotifyApi.getMe()
 
-    }).then(function (data) {
-        spotifyApi
-            .getMyTopTracks({ limit: 50 })
-            .then(function (data) {
-                return data.body.items.map(track => {
-                    // if (!track.name.includes(" ")) {
+    }, error => console.log(2, error))
+    .then(function (data) {
+        console.log(data)
+        return  spotifyApi
+            .getMySavedTracks({ limit: 10, offset: 1 })
+    })
+    .then(console.log, console.log)
+})
+        //     .then(function (data) {
+        //         return data.body.items.map(track => {
+        //             // if (!track.name.includes(" ")) {
                       
-                    return track.id
-                // }
-            })
-            .then(function (trackIds) {
-                return spotifyApi.getAudioFeaturesForTracks(trackIds)
-            })
-            .then(function (data) {
-                data.body.audio_features.forEach((track) => {
-                    // song_name = info_medium[idx][track.id][0]
-                    // artist_name = info_medium[idx][track.id][1]
+        //             return track.id
+        //         // }
+        //     })
+        //     .then(function (trackIds) {
+        //         return spotifyApi.getAudioFeaturesForTracks(trackIds)
+        //     })
+        //     .then(function (data) {
+        //         data.body.audio_features.forEach((track, idx) => {
+        //             song_name = track_info[idx][track.id][0]
+        //             artist_name = track_info[idx][track.id][1]
                     // if (idx <= 4) {
-                        // seeds_medium.push(track.id)
+                    //     seeds_medium.push(track.id)
                     // }
-                    audio_features({
-                        "title": song_name,
-                        "id": track.id,
-                        "artist": artist_name,
+                    // audio_features.push({
+                    //     "title": song_name,
+                    //     "id": track.id,
+                    //     "artist": artist_name,
                         // "idx": idx,
                         //how danceable a song is 0-1
-                        "danceability": track.danceability,
+                        // "danceability": track.danceability,
                         //perceptual measure of intensity and activity
                         //fast, loud, noisy -> 1
-                        "energy": track.energy,
-                        "key": track.key,
+                        // "energy": track.energy,
+                        // "key": track.key,
                         //loudness in decibels, between -60 and 0db
-                        "loudness": track.loudness,
-                        "mode": track.mode,
+                        // "loudness": track.loudness,
+                        // "mode": track.mode,
                         //0-1, 0.5-1 being instrumentals w/ increasing confidence
-                        "instrumentalness": track.instrumentalness,
+                        // "instrumentalness": track.instrumentalness,
                         //musical positiveness 0-1, sad, depressed, angry -> happy, cheerful, euphoric
-                        "valence": track.valence,
+                        // "valence": track.valence,
                         //averaged tempo for a song
-                        "tempo": track.tempo,
-                    })
-                })
-            })
-        })
+                        // "tempo": track.tempo,
+                    // })
+            //     })
+            // })
+        // })
 
-    }).catch(error => console.log(error))
+    // }).catch(error => console.log(error))
 
-})
+// })
 
    
     // const access_url = "https://accounts.spotify.com/api/token";
