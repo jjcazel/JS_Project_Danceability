ef985e3d83b3ba3c1467d5f791ede50a44f7ddcc
JJCazel <jjcazel@gmail.com>
2019-11-26 18:18:10 -0500

    Change redirect uri to work for Heroku

--- API/spotify_api_calls.js
+++ API/spotify_api_calls.js
@@ -1,16 +1,16 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 const querystring = require('querystring');
-// const clusterMaker = require('clusters');
+const clusterMaker = require('clusters');
 
 
-const redirect_uri = 'http://localhost:8000/callback'
+var client_id = '';
+var client_secret = '';
+var redirect_uri = 'https://danceability1.herokuapp.com/callback'
+var stateKey = 'spotify_auth_state';
 // let client_id = 'efe8564cdab24aeda7bf97b81c57683d';
 // let client_secret = '01ae9dd3d2204d35886d7012f6c32540';
-let client_id = '';
-let client_secret = '';
-let stateKey = 'spotify_auth_state';
 
-
+              
 module.exports.spotifyLogin = function (res) {
   let state = generateRandomString(16);
   res.cookie(stateKey, state);
