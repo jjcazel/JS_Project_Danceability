a0b4c7dad7260225d37cf16f9feb34e884add5d4
JJCazel <jjcazel@gmail.com>
2019-11-27 16:07:21 -0500

    Update client_id

--- API/spotify_api_calls.js
+++ API/spotify_api_calls.js
@@ -4,11 +4,11 @@ const querystring = require('querystring');
 
 
 // var client_id = '';
-var client_secret = '';
+// var client_secret = '';
 var redirect_uri = 'https://danceability1.herokuapp.com/callback'
 var stateKey = 'spotify_auth_state';
 let client_id = 'efe8564cdab24aeda7bf97b81c57683d';
-// let client_secret = '01ae9dd3d2204d35886d7012f6c32540';
+let client_secret = '01ae9dd3d2204d35886d7012f6c32540';
 
               
 module.exports.spotifyLogin = function (res) {
