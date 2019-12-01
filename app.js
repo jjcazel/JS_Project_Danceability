330e3060fae9b9e30b2e597138226e6edc37c358
JJCazel <jjcazel@gmail.com>
2019-12-01 15:58:23 -0500

    Work on modals

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const index = require('./src/index');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.favicon(path.join(__dirname, 'src', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
