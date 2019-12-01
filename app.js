6e30ff832732283438651ac4a73bde3e6a87d93f
JJCazel <jjcazel@gmail.com>
2019-12-01 17:29:04 -0500

    Work on modals

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const path = require("path");
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressSession = require('express-session');
+const favicon = require('serve-favicon');
 const index = require('./src/index');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon(path.join(__dirname, 'src', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'src', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
